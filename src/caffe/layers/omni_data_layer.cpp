61953daa3bd76893ba0f88b0cd1b0701dc9f116d
rwduzhao <rw.du.zhao@gmail.com>
2015-03-22 12:08:21 +0800

    add empty src/caffe/layers/omni_data_layer.cpp

---
+++ src/caffe/layers/omni_data_layer.cpp
@@ -0,0 +1,31 @@
+#include <fstream>  // NOLINT(readability/streams)
+#include <iostream>  // NOLINT(readability/streams)
+#include <string>
+#include <utility>
+#include <vector>
+
+#include "caffe/data_layers.hpp"
+#include "caffe/layer.hpp"
+#include "caffe/util/io.hpp"
+#include "caffe/util/math_functions.hpp"
+#include "caffe/util/rng.hpp"
+
+namespace caffe {
+
+template <typename Dtype>
+OmniDataLayer<Dtype>::~OmniDataLayer<Dtype>() {
+  this->JoinPrefetchThread();
+}
+
+template <typename Dtype>
+void OmniDataLayer<Dtype>::DataLayerSetUp(const vector<Blob<Dtype>*>& bottom, vector<Blob<Dtype>*>* top) {
+}
+
+// this function is used to create a thread that prefetches the data.
+template <typename Dtype>
+void OmniDataLayer<Dtype>::InternalThreadEntry() {
+}
+
+INSTANTIATE_CLASS(OmniDataLayer);
+
+}  // namespace caffe
